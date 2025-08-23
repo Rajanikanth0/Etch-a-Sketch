aea388bba7304b97368bc9f1986106c2c5138a03
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-08-23 20:41:55 +0530

    feat: Add 4x4 grid

--- script/logic.js
+++ script/logic.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+function getDivs(count) {
+  const squareBox = document.createDocumentFragment();
+
+  for (let n = 1; n <= count; n++) {
+    const square = document.createElement("div");
+    square.setAttribute("class", "square square" + n);
+
+    squareBox.appendChild(square);
+  }
+
+  return squareBox;
+}
+
+const container = document.querySelector("#container");
+container.appendChild( getDivs(16) );
